0135d8c10ae5a7ea543804b282cc9a48fa5443e1
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-12-12 12:23:57 +0300

    EPMRPP-97593 copyright added

--- src/test/java/com/epam/reportportal/extension/slack/event/launch/SlackLaunchFinishEventListenerTest.java
+++ src/test/java/com/epam/reportportal/extension/slack/event/launch/SlackLaunchFinishEventListenerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.reportportal.extension.slack.event.launch;
 
 import static com.epam.reportportal.extension.slack.utils.SampleAttachment.SAMPLE_ATTACHMENT;
@@ -25,6 +41,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author <a href="mailto:siarhei_hrabko@epam.com">Siarhei Hrabko</a>
+ */
 @ExtendWith(MockitoExtension.class)
 class SlackLaunchFinishEventListenerTest {
 
