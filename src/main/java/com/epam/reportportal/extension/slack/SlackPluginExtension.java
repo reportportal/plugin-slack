2dc23be4af520205f4b7b2d2657e0608aaf6128b
APiankouski <109206864+APiankouski@users.noreply.github.com>
2024-12-12 13:44:31 +0300

    Release 1.0.1 (#10)

--- src/main/java/com/epam/reportportal/extension/slack/SlackPluginExtension.java
+++ src/main/java/com/epam/reportportal/extension/slack/SlackPluginExtension.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.reportportal.extension.slack;
 
 import com.epam.reportportal.extension.CommonPluginCommand;
@@ -47,6 +63,7 @@ import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Andrei Piankouski
@@ -100,6 +117,9 @@ public class SlackPluginExtension implements ReportPortalExtensionPoint, Disposa
   @Autowired
   private ProjectRepository projectRepository;
 
+  // @Autowired // uncomment for future release
+  private final RestTemplate restTemplate = new RestTemplate();
+
   @Autowired
   private ApplicationContext applicationContext;
 
@@ -131,7 +151,7 @@ public class SlackPluginExtension implements ReportPortalExtensionPoint, Disposa
 
     launchFinishEventListenerSupplier = new MemoizingSupplier<>(
         () -> new SlackLaunchFinishEventListener(projectRepository,
-            launchRepository, senderCaseMatcher.get(), attachmentResolverSupplier.get()));
+            launchRepository, senderCaseMatcher.get(), attachmentResolverSupplier.get(), restTemplate));
   }
 
   @PostConstruct
